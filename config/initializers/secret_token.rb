a2716b0b1fc7a8270204bdab25ec0a180ddd60d6
Francesco Valente <francesco83.valente@gmail.com>
2012-10-10 15:20:45 +0200

    Switched to mysql.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PracticeRails3Todomgr::Application.config.secret_token = '400647369b20b1f7953d141a03d61ad5d086cc0a5fecfb1193c8207b93ba3bb6875687c7e0a23ae3d394366f7a46398e0591991cb96d611532efa8bf52a45d5e'
+PracticeRails3Todomgr::Application.config.secret_token = '4068e572dc635eb6e5e6a9b49ac32b99223341955c160c404802c82a3d2b5583d28103759b47c7bbb6569343b0e45d8442d33b8ab1e00a430f92da6fc4dd9883'
